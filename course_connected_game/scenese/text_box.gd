1d8e3eb6e541653c6ee72cf1c792efaf8a231821
Ариадна Чёрненькая <144812396+Ariiaadna@users.noreply.github.com>
2025-10-21 22:55:28 +0300

    Вывод "думает..." с анимацией
    Пока модель думает она думает....

--- course_connected_game/scenese/text_box.gd
+++ course_connected_game/scenese/text_box.gd
@@ -14,7 +14,15 @@ var pun_time = 0.2
 signal finished_displaying()
 
 func display_text(text_to_display: String) -> void:
+	#text = text_to_display
+	#label.text = text_to_display
+	
+	# Сбрасываем состояние при каждом новом выводе
 	text = text_to_display
+	letter_index = 0
+	timer.stop()
+	
+	# Сначала ставим полный текст (нужно для пересчёта размера)
 	label.text = text_to_display
 	
 	await resized # ожидание сигнала об изменении размера тектового поля
@@ -29,14 +37,20 @@ func display_text(text_to_display: String) -> void:
 	global_position.x -= size.x / 2 #центрируем его по горизонтале
 	global_position.y -= size.y + 24 #чтобы он был немного выше говорящего 
 	
+	
 	label.text = ""
 	_display_letter()
 				
 func _display_letter():
+		# Защита от выхода за границы (на случай, если text пуст)
+		if letter_index < 0 or letter_index >= text.length():
+			finished_displaying.emit()
+			return
+			
 		label.text += text[letter_index]
 		
 		letter_index += 1
-		if letter_index >= text.length():
+		if letter_index >= text.length() :
 			finished_displaying.emit() #посылаем сигнал что всё 
 			return
 			
